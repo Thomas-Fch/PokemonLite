76ba44ca1c60e8a5cea993d02fed90559a58b93d
ornel77 <t.ornel@yahoo.fr>
2023-04-07 17:04:20 +0200

    fetch api (Ornella)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+      .then((response) => response.json())
+      .then((data) => console.info(data.results));
+  });
+
   return (
     <div className="App">
       <Home />
